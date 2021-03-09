2fbccd1db3493f5182dcc27c739a982f7efc9a3a
Hazel <65731474+hazelsah@users.noreply.github.com>
2021-03-08 22:57:15 -0800

    Update segmentation.sh

--- segmentation.sh
+++ segmentation.sh
@@ -1,2 +1,3 @@
+# taken from pyscene-detect documentation 
 scenedetect -i video_path -s my_video.stats.mp4 detect-content -t threshold_number split-video
 #e.g.  scenedetect -i video.mp4 -s my_video.stats.mp4 detect-content -t 12 split-video
